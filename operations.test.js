367adee9355fa354c054bedce50d8c6911e4ee6a
chrissan124 <89790594+chrissan124@users.noreply.github.com>
2021-09-29 21:05:53 -0400

    Repositorio Actualizado

--- operations.test.js
+++ operations.test.js
@@ -1,21 +1,19 @@
 const operations = require('./operations');
 
-// STATUS TEST
-test('SUCCESS STATUS', () => {
-  const status = {message: 'HOLA MUNDO (SERVIDOR ACTIVO)', status: 200}
-  expect(status).toBe(status);
-});
-
-test('ERROR STATUS', () => {
-  const status = {message: 'HOLA MUNDO (SERVIDOR ACTIVO)', status: 200}
-  expect({}).not.toBe(status);
-});
-
 // TEST FOR SUM OPERATION
 test('SUCCESS SUM', () => {
   expect(operations.sumOperation(5, 7)).toBe(12);
 });
 
 test('ERROR SUM', () => {
+  expect(operations.sumOperation("a", 7)).toBeNaN();
+});
+
+// TEST FOR MINUS OPERATION
+test('SUCCESS MINUS', () => {
+  expect(operations.minusOperation(10, 5)).toBe(5);
+});
+
+test('ERROR MINUS', () => {
   expect(operations.sumOperation("a", 7)).toBeNaN();
 });
